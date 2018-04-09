a4e1bf10056bf05c08f81a91b1cf73591462b3ae
Maxime CORBIN <maxime.corbin@epitech.eu>
2018-04-09 23:49:19 +0200

    Minor updates + added label support in database

--- gotasks.sql
+++ gotasks.sql
@@ -9,8 +9,8 @@ CREATE TABLE `user` (
   `username` TEXT NOT NULL,
   `password`  TEXT NOT NULL,
   `email` TEXT NOT NULL,
-  `first name` TEXT NOT NULL,
-  `name` TEXT NOT NULL,
+  `first_name` TEXT NOT NULL,
+  `last_name` TEXT NOT NULL,
   PRIMARY KEY (user_id),
 );
 
@@ -38,22 +38,31 @@ CREATE TABLE `user_has_task` (
   `fk_user_id` INT NOT NULL,
   `fk_task_id` INT NOT NULL,
   PRIMARY KEY (fk_user_id, fk_task_id),
-  FOREIGN KEY (fk_user_id) REFERENCES user(user_id),
-  FOREIGN KEY (fk_task_id) REFERENCES task(task_id),
+  FOREIGN KEY (fk_user_id) REFERENCES user(user_id) ON DELETE CASCADE,
+  FOREIGN KEY (fk_task_id) REFERENCES task(task_id) ON DELETE CASCADE,
 )
 
 CREATE TABLE `user_has_project` (
   `fk_user_id` INT NOT NULL,
   `fk_project_id` INT NOT NULL,
   PRIMARY KEY (fk_project_id, fk_user_id),
-  FOREIGN KEY (fk_user_id) REFERENCES user(user_id),
-  FOREIGN KEY (fk_project_id) REFERENCES project(project_id),
+  FOREIGN KEY (fk_user_id) REFERENCES user(user_id) ON DELETE CASCADE,
+  FOREIGN KEY (fk_project_id) REFERENCES project(project_id) ON DELETE CASCADE,
 )
 
-CREATE TABLE `project_has_task` (
-  `fk_project_id` INT NOT NULL,
+CREATE TABLE `label` (
+  `label_id` INT NOT NULL,
+  `color` TEXT NOT NULL,
+  `name` TEXT NOT NULL,
+  `project_id` INT NOT NULL,
+  PRIMARY KEY (label_id),
+  FOREIGN KEY (project_id) REFERENCES project(project_id) ON DELETE CASCADE,
+)
+
+CREATE TABLE `task_has_label` (
   `fk_task_id` INT NOT NULL,
-  PRIMARY KEY (fk_project_id, fk_task_id),
-  FOREIGN KEY (fk_project_id) REFERENCES project(project_id),
-  FOREIGN KEY (fk_task_id) REFERENCES task(task_id),
+  `fk_label_id` INT NOT NULL,
+  PRIMARY KEY (fk_task_id, fk_label_id),
+  FOREIGN KEY (fk_task_id) REFERENCES task(task_id) ON DELETE CASCADE,
+  FOREIGN KEY (fk_label_id) REFERENCES label(label_id) ON DELETE CASCADE,
 )
