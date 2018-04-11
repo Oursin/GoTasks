765252d2dac25917aa758244068b9df2eacddb90
Maxime CORBIN <maxime.corbin@epitech.eu>
2018-04-11 11:12:15 +0200

    Minor sql file update

--- gotasks.sql
+++ gotasks.sql
@@ -11,14 +11,14 @@ CREATE TABLE `user` (
   `email` TEXT NOT NULL,
   `first_name` TEXT NOT NULL,
   `last_name` TEXT NOT NULL,
-  PRIMARY KEY (user_id),
+  PRIMARY KEY (user_id)
 );
 
 CREATE TABLE `project` (
   `project_id` INT NOT NULL AUTO_INCREMENT,
   `title` TEXT NOT NULL,
   `desc` TEXT NOT NULL,
-  PRIMARY KEY (project_id),
+  PRIMARY KEY (project_id)
 );
 
 CREATE TABLE `task` (
@@ -31,7 +31,7 @@ CREATE TABLE `task` (
   `creator_id` INT NOT NULL,
   PRIMARY KEY (`task_id`),
   FOREIGN KEY (project_id) REFERENCES project(project_id) ON DELETE CASCADE,
-  FOREIGN KEY (creator_id) REFERENCES user(user_id),
+  FOREIGN KEY (creator_id) REFERENCES user(user_id)
 );
 
 CREATE TABLE `user_has_task` (
@@ -39,16 +39,16 @@ CREATE TABLE `user_has_task` (
   `fk_task_id` INT NOT NULL,
   PRIMARY KEY (fk_user_id, fk_task_id),
   FOREIGN KEY (fk_user_id) REFERENCES user(user_id) ON DELETE CASCADE,
-  FOREIGN KEY (fk_task_id) REFERENCES task(task_id) ON DELETE CASCADE,
-)
+  FOREIGN KEY (fk_task_id) REFERENCES task(task_id) ON DELETE CASCADE
+);
 
 CREATE TABLE `user_has_project` (
   `fk_user_id` INT NOT NULL,
   `fk_project_id` INT NOT NULL,
   PRIMARY KEY (fk_project_id, fk_user_id),
   FOREIGN KEY (fk_user_id) REFERENCES user(user_id) ON DELETE CASCADE,
-  FOREIGN KEY (fk_project_id) REFERENCES project(project_id) ON DELETE CASCADE,
-)
+  FOREIGN KEY (fk_project_id) REFERENCES project(project_id) ON DELETE CASCADE
+);
 
 CREATE TABLE `label` (
   `label_id` INT NOT NULL,
@@ -56,13 +56,13 @@ CREATE TABLE `label` (
   `name` TEXT NOT NULL,
   `project_id` INT NOT NULL,
   PRIMARY KEY (label_id),
-  FOREIGN KEY (project_id) REFERENCES project(project_id) ON DELETE CASCADE,
-)
+  FOREIGN KEY (project_id) REFERENCES project(project_id) ON DELETE CASCADE
+);
 
 CREATE TABLE `task_has_label` (
   `fk_task_id` INT NOT NULL,
   `fk_label_id` INT NOT NULL,
   PRIMARY KEY (fk_task_id, fk_label_id),
   FOREIGN KEY (fk_task_id) REFERENCES task(task_id) ON DELETE CASCADE,
-  FOREIGN KEY (fk_label_id) REFERENCES label(label_id) ON DELETE CASCADE,
-)
+  FOREIGN KEY (fk_label_id) REFERENCES label(label_id) ON DELETE CASCADE
+);
