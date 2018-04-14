8befac61f474523d52ce28ce93b46d65d07eebca
Maxime CORBIN <maxime.corbin@epitech.eu>
2018-04-15 00:13:19 +0200

    Front end organisation update : Renamed templates to views

--- src/handlers/path.go
+++ src/handlers/path.go
@@ -2,7 +2,7 @@ package handlers
 
 const (
 	//HTMLPath variable defines the default path for html files
-	HTMLPath = "/site/templates/"
+	HTMLPath = "/site/views/"
 	//ImagesPath variable defines the default path for images files
 	ImagesPath = "/site/static/images/"
 )
