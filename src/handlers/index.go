e4a013423b0102bb2d2461662be638fd30ee2285
Maxime CORBIN <maxime.corbin@epitech.eu>
2018-04-15 12:23:13 +0200

    Updated 404 Error page, added and h1 title Small site folders reorganisation Added a TemplatesPath const variable in path.go

--- src/handlers/index.go
+++ src/handlers/index.go
@@ -10,11 +10,12 @@ import (
 // Index : Handles the main page of the site
 func Index(w http.ResponseWriter, _ *http.Request) {
 	base, _ := os.Getwd()
-	path := base + HTMLPath + "index.html"
+	path := base + "/site/index.html"
 	file, err := os.Open(path)
 	if err != nil {
 		log.Print("Error loading index.html")
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
 	}
 	_, err = io.Copy(w, file)
 	if err != nil {
--- src/handlers/path.go
+++ src/handlers/path.go
@@ -3,6 +3,8 @@ package handlers
 const (
 	//HTMLPath variable defines the default path for html files
 	HTMLPath = "/site/views/"
+	//TemplatesPath variable defines the default path for templates
+	TemplatesPath = HTMLPath + "templates/"
 	//ImagesPath variable defines the default path for images files
 	ImagesPath = "/site/static/images/"
 )
